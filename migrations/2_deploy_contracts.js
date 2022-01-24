43d84ce19cc868c1f9c202a605523122e0d3f464
Diego Sanchez Mariñ0 <diegosm@Diegos-MacBook-Pro.local>
2022-01-24 12:05:59 -0300

    First commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+// const LPToken = artifacts.require("LPToken");
+// const UnderlyingToken = artifacts.require("UnderlyingToken");
+// const LiquitidyPool = artifacts.require("LiquitidyPool");
+
+module.exports = function (deployer) {
+    // deployer.deploy(LPToken);
+    // deployer.deploy(UnderlyingToken);
+    // deployer.deploy(LiquitidyPool);
+};
