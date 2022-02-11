5da2d20b5a0aca2c1e361e556312d30ea5dc57ad
Diego Sanchez Mariñ0 <diegosm@Diegos-MacBook-Pro.local>
2022-02-11 17:06:25 -0300

    Added hardhat as deployment tool

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,9 +12,7 @@ module.exports = function (deployer) {
     // deployer.deploy(Token)
 
     deployer.deploy(Factory)
-    deployer.deploy(Token).then(function () {
-        return deployer.deploy(Exchange, Token.address)
-    })
+    deployer.deploy(Token)
 
     // deployer.deploy(Factory, Token.address).then(function () {
     // deployer.deploy(Exchange, Token.deployed().address)
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+
+const hre = require("hardhat");
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    // const Factory = await hre.ethers.getContractFactory("Factory");
+    // const Token = await hre.ethers.getContractFactory("Token");
+
+
+    // const factory = await Factory.deploy();
+    // const token = await Token.deploy();
+
+    // console.log("Factory address:", factory.address);
+    // console.log("Token address:", token.address);
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
