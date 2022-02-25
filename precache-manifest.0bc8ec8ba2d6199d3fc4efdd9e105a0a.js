2d13e957fe6026d1b0cc098244576864bfac3603
Diego Sanchez Mariñ0 <diegosm@Diegos-MacBook-Pro.local>
2022-02-25 12:23:05 -0300

    Updates

---
+++ precache-manifest.0bc8ec8ba2d6199d3fc4efdd9e105a0a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2ef8ca418af8e633703",
+    "url": "/DEX/static/js/main.d2ef8ca4.chunk.js"
+  },
+  {
+    "revision": "d04d068e4ffb2a8b1ac5",
+    "url": "/DEX/static/js/1.d04d068e.chunk.js"
+  },
+  {
+    "revision": "9a2d880803809322ebdc",
+    "url": "/DEX/static/js/runtime~main.9a2d8808.js"
+  },
+  {
+    "revision": "4962edf7a989acd8ed5e8c71eb7e3cd4",
+    "url": "/DEX/index.html"
+  }
+];
